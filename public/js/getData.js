39ef9df11c679e4b972503a8b22139b438da02f8
Drslx <drslxprogramador@gmail.com>
2023-03-19 11:23:18 -0300

    adicionado a funcao getData()

---
+++ public/js/getData.js
@@ -0,0 +1,16 @@
+/* Url API */
+const url = "http://localhost:3001/summarys";
+
+/* Funcao assincrona para pegar dados */
+async function getDate() {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  
+  //console.log(data);
+  return console.log(data);
+}
+
+/* Chamada da funcao */
+getDate();
+
