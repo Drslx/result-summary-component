95bc3d39461b2bdefe497419a63032e54dd93bb2
Drslx <drslxprogramador@gmail.com>
2023-04-07 07:15:23 -0300

    alterando local index.html

--- public/js/getDate.js
+++ public/js/getDate.js
@@ -1,12 +1,11 @@
 /* Url API */
 const url = "http://localhost:3001/summarys";
 
-/* Funcao assincrona para pegar dados */
 async function getDate() {
   const res = await fetch(url);
   const data = await res.json();
+
   return data;
 }
-/* Chamada da funcao */
-getDate();
 
+getDate();
